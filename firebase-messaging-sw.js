9faa5824ecc079d237c36931f5ad3d222dbd421e
uib2usbt <44798885+uib2usbt@users.noreply.github.com>
2018-11-06 15:18:43 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/5.5.6/firebase-app.js');
-importScripts('/__/firebase/5.5.6/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.7/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.7/firebase-messaging.js');
+/*importScripts('/__/firebase/init.js');*/
 
 var messaging = firebase.messaging();
 
