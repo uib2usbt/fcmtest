9f2f00325a6ad0da4451171bd9bb922558e7139d
uib2usbt <44798885+uib2usbt@users.noreply.github.com>
2018-11-06 15:15:15 +0500

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,39 +1,50 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+// Import and configure the Firebase SDK
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+importScripts('/__/firebase/5.5.6/firebase-app.js');
+importScripts('/__/firebase/5.5.6/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
 
-firebase.initializeApp({
-  messagingSenderId: '448358493027'
-});
+var messaging = firebase.messaging();
 
-const messaging = firebase.messaging();
+/**
+ * Here is is the code snippet to initialize Firebase Messaging in the Service
+ * Worker when your app is not hosted on Firebase Hosting.
 
-// Customize notification handler
+ // [START initialize_firebase_in_sw]
+ // Give the service worker access to Firebase Messaging.
+ // Note that you can only use Firebase Messaging here, other Firebase libraries
+ // are not available in the service worker.
+ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+ // Initialize the Firebase app in the service worker by passing in the
+ // messagingSenderId.
+ firebase.initializeApp({
+   'messagingSenderId': 'YOUR-SENDER-ID'
+ });
+
+ // Retrieve an instance of Firebase Messaging so that it can handle background
+ // messages.
+ const messaging = firebase.messaging();
+ // [END initialize_firebase_in_sw]
+ **/
+
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
 messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('Handling background message', payload);
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  var notificationTitle = 'Background Message Title';
+  var notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
 
-  // Copy data object to get parameters in the click handler
-  payload.data.data = JSON.parse(JSON.stringify(payload.data));
-
-  return self.registration.showNotification(payload.data.title, payload.data);
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
-
-self.addEventListener('notificationclick', function(event) {
-  const target = event.notification.data.click_action || '/';
-  event.notification.close();
-
-  // This looks to see if the current is already open and focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: 'window',
-    includeUncontrolled: true
-  }).then(function(clientList) {
-    // clientList always is empty?!
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === target && 'focus' in client) {
-        return client.focus();
-      }
-    }
-
-    return clients.openWindow(target);
-  }));
-});
+// [END background_handler]
